8b4d0eedf091851e8267ce01c9d47be1cdd0b3a0
binu manoharan <binu1988@gmail.com>
2016-09-07 06:00:08 +0100

    Make heroes be an object array. Let there be more heroes!

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,6 +1,40 @@
 import { Component } from '@angular/core';
 @Component({
   selector: 'my-app',
-  template: '<h1>My First Angular 2 App</h1>'
+  template: `
+  <h1>{{title}}</h1>
+  <h2>{{heroes[0].name}} details!</h2>
+  <div><label>id: </label>{{heroes[0].id}}</div>
+  <div>
+    <label>name: </label>
+    <input value="{{heroes[0].name}}" placeholder="name">
+  </div>
+  <div>
+    <p *ngIf="heroes.length > 3">There are many heroes!</p>
+    <ul>
+      <li *ngFor="let hero of heroes">
+        {{ hero.name }}
+      </li>
+    </ul>
+  </div>
+  `
 })
-export class AppComponent { }
+
+export class AppComponent {
+  title = "Tour of Heroes";
+  heroes = [
+    new Hero(1, "Maiev Shadowsong"),
+    new Hero(2, "Illidan Stormrage")
+    new Hero(3, "Arthus")
+    new Hero(4, "Priestess of the Moon")
+    new Hero(5, "Thrall")
+  ]
+}
+
+export class Hero {
+  constructor (
+    public id: number,
+    public name: string
+  ) {}
+}
+
